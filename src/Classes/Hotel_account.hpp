28ba7776695f4ca4ad1fd35abc6363fff93a85bb
Konrad Marcinkowski <km.dartmoor@gmail.com>
2024-11-04 09:41:38 +0100

    started hotel

--- src/Classes/Hotel_account.hpp
+++ src/Classes/Hotel_account.hpp
@@ -4,11 +4,16 @@
 
 #ifndef HOTEL_ACCOUNT_HPP
 #define HOTEL_ACCOUNT_HPP
+#include <array>
 
-
+struct Room {
+    unsigned int room_id;
+    bool is_vacant;
+    double room_cost;
+};
 
 class Hotel_account {
-
+    std::array<std::tuple<Room, struct tm, struct tm>, 50>;
 };
 
 
