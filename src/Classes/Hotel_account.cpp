6de61bbe53ed0d01de6fb9c85cb7b4e5bf5584fa
Konrad Marcinkowski <km.dartmoor@gmail.com>
2024-11-04 09:10:58 +0100

    rel

---
+++ src/Classes/Hotel_account.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Konrad Marcinkowski on 04/11/2024.
+//
+
+#include "Hotel_account.hpp"
---
+++ src/Classes/Hotel_account.hpp
@@ -0,0 +1,16 @@
+//
+// Created by Konrad Marcinkowski on 04/11/2024.
+//
+
+#ifndef HOTEL_ACCOUNT_HPP
+#define HOTEL_ACCOUNT_HPP
+
+
+
+class Hotel_account {
+
+};
+
+
+
+#endif //HOTEL_ACCOUNT_HPP
