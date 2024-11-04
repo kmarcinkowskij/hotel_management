ae36a7557fc6823725690e6b609b0d1132c47638
Konrad Marcinkowski <km.dartmoor@gmail.com>
2024-11-04 10:09:02 +0100

    added user_id

--- src/Classes/User_account.hpp
+++ src/Classes/User_account.hpp
@@ -11,6 +11,7 @@
 
 class User_account {
 private:
+    unsigned int user_id;
     double accumulated_cost;
     time_t booked_in;
     time_t booked_out;
@@ -18,14 +19,19 @@ private:
     unsigned int user_booked_room_id;
     std::vector<Service> chosen_services;
 public:
-    User_account(const double &_accumulated_cost, const time_t &_booked_in, const time_t &_booked_out, const unsigned int &_booked_nights,
+    User_account(const unsigned int &_user_id, const double &_accumulated_cost, const time_t &_booked_in, const time_t &_booked_out, const unsigned int &_booked_nights,
         const unsigned int &_user_booked_room_id, const std::vector<Service> &_chosen_services)
-        : accumulated_cost(_accumulated_cost),
+        : user_id(_user_id),
+          accumulated_cost(_accumulated_cost),
           booked_in(_booked_in),
           booked_out(_booked_out),
           booked_nights(_booked_nights),
           user_booked_room_id(_user_booked_room_id),
           chosen_services(_chosen_services) {
+    }
+
+    [[nodiscard]] unsigned int get_user_id() const {
+        return user_id;
     }
 
     [[nodiscard]] double get_accumulated_cost() const {
