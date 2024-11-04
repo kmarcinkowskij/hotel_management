a9c03393ec520bd7842b802dbbeadf595107f963
lukasz-stepien-dev <lukaszatepien@gmail.com>
2024-11-04 10:35:03 +0100

    Refactor docs of Amenities class, now docs match for standards

--- src/Classes/Amenities.hpp
+++ src/Classes/Amenities.hpp
@@ -1,6 +1,7 @@
-//
-// Created by Łukasz Stępień on 04/11/2024.
-//
+// Plik: Amenities.hpp
+// Autor: Łukasz Stępień
+// Data: 04 listopada 2024
+// Opis: Definicja struktury Amenities, która przechowuje dane dotyczące udogodnień dostępnych w hotelu.
 
 #ifndef AMENITIES_HPP
 #define AMENITIES_HPP
@@ -14,11 +15,9 @@ struct Amenities {
   std::string amenity_description;
   double amenity_price;
 
+  // Konstruktor inicjalizuje atrybuty amenity_id, amenity_name, amenity_description oraz amenity_price podanymi wartościami.
   Amenities(unsigned int id, const std::string& name, const std::string& description, double price)
   : amenity_id(id), amenity_name(name), amenity_description(description), amenity_price(price) {}
-
 };
-
-
 
 #endif //AMENITIES_HPP
